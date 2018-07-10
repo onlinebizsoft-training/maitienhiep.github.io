9f618c556610f3569bbaf58bc8f394075bcae319
Mai Hiep <hiepknor@gmail.com>
2018-07-10 12:00:36 +0700

    Add effect, make sticky header on scroll  for nav. Make "back to top" button.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   showMenu();
   slideShow();
+  scrollFunction();
+  stickyHeader();
 });
+
+// Hàm ẩn/hiện menu
 function showMenu() {
   $(".left-menu-1").click(function() {
     if ($(this).hasClass("active")) {
@@ -34,33 +38,101 @@ function showMenu() {
   });
 }
 
+// Create action for slideshow.
 function slideShow() {
+  // Khởi tạo một biến số thứ tự = 0.
   var stt = 0;
+  // Một biến mới được gán cho ảnh bắt đầu của slide.
   startImg = $("img.slide:first").attr("stt");
+  // Một biến mới được gán cho ảnh kết thúc của slide.
   endImg = $("img.slide:last").attr("stt");
+  // Khởi tạo vòng lặp qua từng ảnh có trong slide.
   $("img.slide").each(function() {
-    if ($(this).is(":visible"))
-      stt = $(this).attr("stt");
+    if ($(this).is(":visible")) stt = $(this).attr("stt");
   });
-  $(".next").click(function(){
-    if(stt == endImg){
+  // Khởi tạo hành động cho next button.
+  $(".next").click(function() {
+    if (stt == endImg) {
       stt = -1;
     }
     next = ++stt;
     $("img.slide").hide();
-    $("img.slide").eq(next).show();
-  })
-  $(".prev").click(function(){
-    if(stt == 0){
+    $("img.slide")
+      .eq(next)
+      .show();
+  });
+  // Khởi tạo hành động cho prev button.
+  $(".prev").click(function() {
+    if (stt == 0) {
       stt = endImg;
       prev = stt++;
     }
     prev = --stt;
     $("img.slide").hide();
-    $("img.slide").eq(prev).show();
-  })
-  setInterval(function(){
-    $(".next").click()
+    $("img.slide")
+      .eq(prev)
+      .show();
+  });
+  // Lặp lại hành động.
+  setInterval(function() {
+    $(".next").click();
   }, 5000);
 }
 
+// Scroll to top function
+function scrollFunction() {
+  // Đặt điều kiện ẩn/hiện scroll button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $(".scrollToTop").fadeIn();
+    } else {
+      $(".scrollToTop").fadeOut();
+    }
+  });
+
+  // Đặt hành động khi click vào scroll button
+  $(".scrollToTop").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+}
+
+// Sticky header on scroll
+function stickyHeader() {
+  // Xác định, kiểm tra điều kiện kích thước trình duyệt để tùy từng giao diện hiển thị nav khác nhau.
+  if(window.innerWidth < 768){
+    $(window).scroll(function() {
+      if ($(window).scrollTop() >= 366.66) {
+        $(".left-menu").addClass("fixed-header");
+        $(this).addClass("visible-title");
+      } else {
+        $(".left-menu").removeClass("fixed-header");
+        $(this).removeClass("visible-title");
+      }
+    });
+  } else if(window.innerWidth <= 1024){
+    $(window).scroll(function() {
+      if ($(window).scrollTop() >= 208) {
+        $(".left-menu").addClass("fixed-header");
+        $(this).addClass("visible-title");
+      } else {
+        $(".left-menu").removeClass("fixed-header");
+        $(this).removeClass("visible-title");
+      }
+    });
+  } else {
+    $(window).scroll(function() {
+      if ($(window).scrollTop() >= 138) {
+        $('.left-menu').addClass("custom");
+        $('.catg-menu').slideUp();
+        $(".nav").addClass("fixed-header");
+        $(this).addClass("visible-title");
+      } else {
+        $('.left-menu').removeClass("custom");
+        $('.catg-menu').slideDown();
+        $(".nav").removeClass("fixed-header");
+        $(this).removeClass("visible-title");
+      }
+    });
+  }
+}
